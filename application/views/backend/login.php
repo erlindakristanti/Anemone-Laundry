2ad3875f7d11f4947b405f30b83dd2b136ae1279
fikri441 <fikriyatulilmi@gmail.com>
2022-06-08 13:00:57 +0700

    Update login.php

--- application/views/backend/login.php
+++ application/views/backend/login.php
@@ -1,16 +1,18 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Login Sistem</title>
+
+    <style>
+        .bg_login{
+             background-image: linear-gradient(#5245c5, #70bce7);
+        }
+    </style>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url()?>assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -19,18 +21,14 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url()?>assets/admin/css/sb-admin-2.css" rel="stylesheet">
-
 </head>
 
-<body class="bg-gradient-primary">
-
+<body class="bg_login">
     <div class="container">
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-xl-10 col-lg-12 col-md-9">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
